b60d1a229aab3959ccd08468d473a39d128307d0
negi <negivishnu58@gmail.com>
2024-08-08 14:55:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useContext } from "react";
+import Sidebar from "./components/sidebar";
+import Player from "./components/Player";
+import Display from "./components/Display";
+import { PlayerContext } from "./context/Playercontext";
+
+
+const App = () => {
+
+    const { audioRef, track } = useContext(PlayerContext);
+    return (
+        <div className="h-screen bg-black">
+            <div className="h-[90%] flex">
+                <Sidebar />
+                <Display />
+            </div>
+            <Player />
+            <audio ref={audioRef} src={track.file} preload="auto" />
+        </div>
+    )
+}
+
+export default App;
